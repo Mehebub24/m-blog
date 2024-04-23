dc3cae0ef58dd33eb141bea91685095b3fdfec9d
Mehebub24 <Mehebubkalapati@gmail.com>
2024-04-23 20:00:16 +0530

    change singup button outline in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,7 +29,7 @@ export default function Header() {
             <FaMoon />
         </Button>
         <Link to='/sing-in'>
-            <Button gradientDuoTone="purpleToBlue">
+            <Button gradientDuoTone="purpleToBlue" outline>
                 SinIn In
             </Button>
         </Link>
